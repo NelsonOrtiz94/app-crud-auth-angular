1bb7d0657d9ee060b7ded8cabca00f121c360f3d
NelsonOrtiz94 <ortnn990@gmail.com>
2024-09-25 12:43:58 -0500

    Se adecuan variables de entorno por temas de seguridad

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,23 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../../environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"ng-task-18-bf043","appId":"1:27434260513:web:b8b4155b68e8032c63eb60","storageBucket":"ng-task-18-bf043.appspot.com","apiKey":"AIzaSyDN8SWwYWRzE4EoRZJQYq0wkRP_yt1Zz_E","authDomain":"ng-task-18-bf043.firebaseapp.com","messagingSenderId":"27434260513"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: environment.firebase.projectId,
+        appId: environment.firebase.appId,
+        storageBucket: environment.firebase.storageBucket,
+        apiKey: environment.firebase.apiKey,
+        authDomain: environment.firebase.authDomain,
+        messagingSenderId: environment.firebase.messagingSenderId,
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
