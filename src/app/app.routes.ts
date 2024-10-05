a727fc8161c2b9dec92dcb56d21ad205e93bf3e7
NelsonOrtiz94 <ortnn990@gmail.com>
2024-10-05 14:38:39 -0500

    Se configura ruta tasks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        loadChildren: () => import('./auth/features/auth.routes')
-    },
-    // {
-    //     path: 'tasks',
-    // }
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/features/auth.routes'),
+  },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./task/features/task.route'),
+  },
+  {
+    path: '**',
+    redirectTo: '/tasks'
+  },
 ];
